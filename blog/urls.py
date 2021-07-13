85d51596967892e83ce88011cb6d58ea78899e7c
Grig0ryyy <parapararammm@gmail.com>
2021-07-13 18:15:02 +0300

    Added sorting of posts, viewing previous posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_blogs, name = 'all_blogs'),
+    path('posts', views.all_posts, name = 'all_posts'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from .models import Blog_entry
 
 def all_blogs(request):
-    projects = Blog_entry.objects.all()
+    projects = Blog_entry.objects.order_by('-data')[:5]
     return render(request, 'blog/all_blogs.html', {'projects':projects})
+
+def all_posts(request):
+    projects = Blog_entry.objects.order_by('-data')
+    return render(request, 'blog/all_posts.html', {'projects':projects})
