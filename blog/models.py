ab916d388bc11fdc80ed6d21fa8cf2a5033ae543
Grig0ryyy <parapararammm@gmail.com>
2021-07-13 17:52:04 +0300

    created the skeleton of the blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Blog_entry(models.Model):
+    title = models.CharField(max_length=100)
+    data = models.DateField(auto_now_add=True)
+    description = models.CharField(max_length=240)
+    url = models.URLField(blank=True)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Blog_entry
 
-# Create your views here.
+def all_blogs(request):
+    projects = Blog_entry.objects.all()
+    return render(request, 'blog/all_blogs.html', {'projects':projects})
