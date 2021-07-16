3b27e2a64ff25d49f81dc4b6fb6f64e99f0f2726
Grig0ryyy <parapararammm@gmail.com>
2021-07-16 14:08:56 +0000

    site was hosted

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&0b5)px728^iui6w1+c8+7ttebgl&)5yo1j#(#)6wtatacr$j!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['grig0ryyy.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
